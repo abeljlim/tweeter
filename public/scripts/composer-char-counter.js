309538020dd04ebd81fd215a321305b692353898
abeljlim <subrosian@hotmail.com>
2023-04-25 20:22:50 -0700

    Implemented char counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  // --- our code goes here ---
+
+  // traverse to get the compose tweet char counter number
+  const origCounterValue = Number($('#tweet-text').parent().find('.counter').html());
+  // console.log(typeof origCounterValue);
+
+  console.log('$(document).ready');
+  $('#tweet-text').on('input', function() {
+    if(origCounterValue === -1) {
+      
+    }
+    // console.log(`#tweet-text length: ${$(this).val().length}`);
+    const textareaLen = $(this).val().length;
+    const $counter = $(this).parent().find('.counter');
+    const totalCharsLeft = origCounterValue - textareaLen;
+    $counter.html(totalCharsLeft);
+    if(totalCharsLeft < 0) {
+      $counter.css("color", "red");
+    }
+    else {
+      // remove style
+      $counter.css("color", '');
+    }
+  });
+});
+console.log('main thread');
