ab74e3ed58af1d61e737fa149da60196fbb85366
abeljlim <subrosian@hotmail.com>
2023-04-26 20:53:48 -0700

    swapped jquery version with a newer one that also has an integrity attribute, and used timeago library instead of stackoverflow code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,29 +5,6 @@
  */
 
 $(document).ready(function() {
-
-  // code from https://stackoverflow.com/questions/6108819/javascript-timestamp-to-relative-time
-  // in miliseconds
-  const units = {
-    year: 24 * 60 * 60 * 1000 * 365,
-    month: 24 * 60 * 60 * 1000 * 365 / 12,
-    day: 24 * 60 * 60 * 1000,
-    hour: 60 * 60 * 1000,
-    minute: 60 * 1000,
-    second: 1000
-  };
-
-  const rtf = new Intl.RelativeTimeFormat('en', { numeric: 'auto' });
-
-  const getRelativeTime = (d1, d2 = new Date()) => {
-    const elapsed = d1 - d2;
-
-    // "Math.abs" accounts for both "past" & "future" scenarios
-    for (const u in units)
-      if (Math.abs(elapsed) > units[u] || u == 'second')
-        return rtf.format(Math.round(elapsed / units[u]), u);
-  };
-
   const createTweetElement = function(tweet) {
 
     // Implementing jQuery HTML data to avoid XSS
@@ -66,7 +43,7 @@ $(document).ready(function() {
     // handle footer data
     // probably won't be able to inject anything here, so will just code it in
     $tweetElement.append($(`<footer>
-    <div class="timestamp">${getRelativeTime(1461116232227)}</div>
+    <div class="timestamp">${timeago.format(1461116232227)}</div>
     <div class="tweet-actions">
       <i class="fa-regular fa-flag"></i>
       <i class="fa-solid fa-retweet"></i>
