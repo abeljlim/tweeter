e16666d8a9d606b788cf590120b079e8502a2b7b
abeljlim <subrosian@hotmail.com>
2023-04-26 18:41:21 -0700

    removed test code and data

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,45 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const tweet = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
-  },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-};
-
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
-
 $(document).ready(function() {
 
   // code from https://stackoverflow.com/questions/6108819/javascript-timestamp-to-relative-time
@@ -119,7 +80,6 @@ $(document).ready(function() {
     console.log('tweetElement.html():', $tweetElement.html());
     return $tweetElement;
   };
-  const $tweet = createTweetElement(tweet);
 
   const renderTweets = function(tweetsArr) {
     const $tweetsContainer = $('#tweets-container');
@@ -196,6 +156,4 @@ $(document).ready(function() {
   
   loadTweets();
   $newTweetForm.on('submit', validateForm);
-  // console.log($tweet);
-  // $('#tweets-container').prepend($tweet);
 });
